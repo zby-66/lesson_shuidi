9387a3bcb14cc1e850bf02fbe3e58db072641633
zby-66 <1753058819@qq.com>
2020-01-03 21:46:18 +0800

    docker

--- docker/node/dockerstudy/readme.md
+++ docker/node/dockerstudy/readme.md
@@ -18,3 +18,11 @@ egg.js 基于koa的二次开发
 - logger
     koa-logger use
 - 出错模块
+- https://img.bosszhipin.com/
+- 静态资源 服务器端资源
+IP
+静态资源
+img.bosszhipin.com
+dns 解析？网络供应商 列表
+公司会在网络供应商机房里去装一些cdn机器
+本地没有？ 去中央服务器里请求一次 缓存在本地
--- docker/node/dockerstudy/routes/index.js
+++ docker/node/dockerstudy/routes/index.js
@@ -4,6 +4,9 @@ router.get('/', async (ctx, next) =>{
     //把模板读取到内存，pug 模板的编译
     await ctx.render('index', {title: '首页'});
 });
-
+router.post('/form', async(ctx, next) =>{
+    console.log('form');
+    ctx.body = ctx.request.body
+});
 //commonjs
 module.exports = router
