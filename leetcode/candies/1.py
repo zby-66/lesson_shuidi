5328486fddbae23034a4927c14f2bac802a8aaaf
zby-66 <1753058819@qq.com>
2020-02-02 10:48:50 +0800

    分糖果

--- leetcode/candies/1.py
+++ leetcode/candies/1.py
@@ -1,4 +1,6 @@
+from typing import List
 class Solution:
     def distributeCandies(self, candies: List[int]) -> int:
         # min 函数式内置函数 
         return min(len(set(candies)), len(candies)>>1) #set 去重
+    
