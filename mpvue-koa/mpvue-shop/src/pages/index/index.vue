add519e0936427b4b9fbec59a43c4feb273e4553
zby-66 <1753058819@qq.com>
2020-02-04 22:28:25 +0800

    网易严选

--- mpvue-koa/mpvue-shop/src/pages/index/index.vue
+++ mpvue-koa/mpvue-shop/src/pages/index/index.vue
@@ -6,6 +6,15 @@
         <input type="text" placeholder="搜索商品">
         <span class="icon"></span>
       </div>
+      <div class="swiper">
+        <swiper class="swiper-container" indicator-dots="true" autoplay ="true" interval="3000" circular="true" duration="500">
+          <block v-for="(item, index) in banner" :key="index">
+            <swiper-item class="swiper-item">
+              <image class="slide-image" :src ="item.image_url"></image>
+            </swiper-item>
+          </block>
+        </swiper>
+      </div>
     </div>
 </div>
 </template>
@@ -13,9 +22,11 @@
 <script>
 import amapFile from "../../utils/amap-wx.js";
 import { mapState, mapMutations } from "vuex";
+import {get} from '../../utils'
 export default {
   data () {
     return {
+        banner: []
       }
     },
     computed: {
@@ -65,6 +76,10 @@ export default {
           _this.update({ cityName: "北京市" });
         }
       });
+    },
+    async getDate(){
+      const data = await get('/index/index')// 后端接口
+      console.log(data);
     },
     toSearch() {
       wx.navigateTo({
--- mpvue-koa/mpvue-shop/src/utils/index.js
+++ mpvue-koa/mpvue-shop/src/utils/index.js
@@ -18,6 +18,50 @@ export function formatTime (date) {
   return `${t1} ${t2}`
 }
 
+
+//--------------------------------------请求的封装
+
+const host = "http://locallhost:5757/lm"
+export {host};
+
+//请求封装
+function request (url, method, data, header = {}){
+  wx.showLoading({
+    title: 'Loading...', //提示的内容,
+    
+  });
+  return new Promise ((resolve, reject) =>{
+    wx.request({
+      url: host +url, //开发者服务器接口地址",
+      data: data, //请求的参数",
+      method: method,
+      header: {
+        "content-type": "application/json"
+      },
+      success (res){
+        wx.hideLoading();
+        resolve(res.data)
+      },
+      fail (error) {
+        wx.hideLoading();
+        reject(false)
+      },
+      complete (){
+        wx.hideLoading();
+      }
+    });
+  })
+}
+
+export function get (url, data){
+  return request(url, 'GET', data)
+}
+
+export function post (url, data){
+  return request(url, 'POST', data)
+}
+
+
 export default {
   formatNumber,
   formatTime
