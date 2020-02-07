98e2b6b01a6a83c9e5d175e4a81ad05387521647
zby-66 <1753058819@qq.com>
2020-02-07 21:24:18 +0800

    debug

--- vue/vue-backend/index.js
+++ vue/vue-backend/index.js
@@ -1,24 +1,30 @@
-const http = require('http');
+const Controller = require("./controller");
+const http = require("http");
 const server = http.createServer();
-const Controller = require('./controller');
 
 const controller = new Controller();
 
-server.on("request", async (req, res) =>{
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
+server.on("request", async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.status = 200;
+    res.end();
+    return;
+  }
+  if (req.url === "/verify") {
+    await controller.handleVerifyUpload(req, res);
+    return;
+  }
 
-    if(req.method === 'OPTIONS') {
-        res.status = 200;
-        res.end();
-        return;
-    }
+  if (req.url === "/merge") {
+    await controller.handleMerge(req, res);
+    return;
+  }
 
-    
-    if(req.url == '/verify') {
-        // res.end('verify');
-        await controller.handleVerifyUpload(req, res)
-    }
-})
+  if (req.url === "/") {
+    await controller.handleFormData(req, res);
+  }
+});
 
-server.listen(3000, () => console.log("正在接近小彭，注意注意！！！"));
+server.listen(3000, () => console.log("正在监听 3000 端口"));
