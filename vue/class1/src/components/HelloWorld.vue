23fb08b84bab36c584ab2afd16b6058031080444
zby-66 <1753058819@qq.com>
2020-02-09 19:07:56 +0800

    ElementUI 底层实现模仿

---
+++ vue/class1/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <button @click="check">点击查看效果</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    check () {
+      this.$toast('网络异常', 'top')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
