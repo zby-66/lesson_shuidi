72c84729b79c008b51e1a2547ee515acaadb2a66
zby-66 <1753058819@qq.com>
2020-03-03 19:51:48 +0800

    全栈项目准备，解决跨域问题，与项目打包，路由路径等等问题

--- vue/ele_unit/ele_admin/src/views/Login.vue
+++ vue/ele_unit/ele_admin/src/views/Login.vue
@@ -1,42 +1,94 @@
 <template>
   <div class="login_page fillcontain">
-    <!-- vue 内置组件  带来特效 当它的子元素  主持人， 报节目 
-    从不显示到显示 v-if v-show都会触发 -->
+    <!-- Vue 内置组件 实现特效 -->
     <transition name="form-fade">
-      <!-- 进场的切换 四个类名 
-        进场 false -> true  想死你们了  冯  暖场  form-fade-enter 
-        短暂时间 
-        元素插入之前生效， 在插入之后的下一frame 移除 
-        form-fade-enter-active 进入的过程之中  transition 时间 
-        .form-fade-enter -> .form-fade-enter-active(1s)
-        出场  true -> false
-        form-fade-leave 离开的开始状态  一frame
-        form-fade-leave-active 离开
-      -->
-      <section class="form_container form-fade-enter" v-show="showLogin">
+      <section class="form_container" v-show="showLogin">
         <div class="manage_tip">
           <p>elm后台管理系统</p>
         </div>
+        <!-- element-ui 表单组件 收集数据 -->
+        <el-form :model="loginForm" :rules="rules" ref="loginForm">
+          <!-- el-form-item 容器 -->
+          <el-form-item prop="username">
+            <el-input v-model="loginForm.username" placeholder="用户名"></el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input v-model="loginForm.password" placeholder="密码" type="password"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('loginForm')" class="submit-btn">登录</el-button>
+          </el-form-item>
+        </el-form>
       </section>
     </transition>
   </div>
 </template>
+
 <script>
+// 后端请求都应该在 api 中
+import {login} from '@/api/getData'
 export default {
+  name: 'Login',
   data() {
     return {
-      showLogin: false
+      showLogin: false,
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
+      }
     }
   },
   mounted() {
     this.showLogin = true
+  },
+  methods: {
+    async submitForm(formName) {
+      // console.log(this.$refs[loginForm])
+      this.$refs[formName].validate(async (valid) => {
+        console.log(valid)
+        if (valid) { // 跟后端 api 通信，登录
+          // 异步 ajax 请求
+          const res = await login({user_name: this.loginForm.username, password: this.loginForm.password})
+          // console.log(res)
+          // 快速模拟 先把能写的代码写完 减少不确定性
+          if (res.status == 1) {
+            this.$message({
+              type: 'success',
+              message: '登录成功'
+            })
+          } else {
+            this.$message({
+              type: 'error',
+              message: res.message
+            })
+          }
+        } else {
+          // this -> component.prototye -> Vue 根实例 -> Vue.use()
+          this.$notify.error({
+            title: '错误',
+            message: '请输入正确的用户名和密码',
+            offset: 100
+          })
+        }
+      })
+    }
   }
 }
 </script>
+
 <style lang="stylus" scoped>
 .login_page
   height 100vh
   background-color #324057
+  
 .manage_tip
   position absolute
   width 100%
@@ -45,31 +97,28 @@ export default {
   p
     font-size 34px
     color #ffffff
-
 .form_container
   width 320px
   height 240px
   position absolute
   top 50%
   left 50%
-  margin-top -160px
-  margin-left -120px
+  box-sizing border-box
   padding 25px
+  margin-top -120px
+  margin-left -160px
   border-radius 5px
   text-align center
-  background-color #ffffff
+  background-color #fff
   .submit_btn
     width 100%
     font-size 16px
-// 动态的产生四个类
-.form-fade-enter-active, .form-fade-leave-active
+// 动态产生的四个类
+.form-fade-enter-active,.form-fade-leave-active
   transition all 1s
-// 刚开始时 透明度为0   原来 opacity 1 设置进入的初始状态
-// 进入到真正进入 最后的样式opacity 1 transform translate3d(0, 0px, 0)
-// - 帧
-// form-fade-leave true false opacity 1 translate3d(0, 0px, 0)
-// opacity 0 translate3d(0, -50px, 0)
-.form-fade-enter, .form-fade-leave-active
+// 刚开始时 透明度为0 设置进入的初始状态
+// 只有一帧的时间
+.form-fade-enter, .form-date-leave-active
   transform translate3d(0, -50px, 0)
-  opacity 0 
+  opacity 0
 </style>
--- vue/ele_unit/ele_web/src/router/index.js
+++ vue/ele_unit/ele_web/src/router/index.js
@@ -17,16 +17,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
   }
 ]
 
 const router = new VueRouter({
-  // mode: 'history',
+  mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
