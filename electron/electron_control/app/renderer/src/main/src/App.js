1c895cdc3a675962d2d7667e30e600b49c4e6470
zby-66 <1753058819@qq.com>
2020-05-22 17:06:45 +0800

    ipc

--- electron/electron_control/app/renderer/src/main/src/App.js
+++ electron/electron_control/app/renderer/src/main/src/App.js
@@ -1,18 +1,54 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-// electron调用NativeAPI 多进程架构
-// Inter-Process Communication 进程间通信
-// electron 不支持import
-// create-react-app 0配置启动
-import { ipcRenderer } from "electron"; // 去到ipc
+import React, { useEffect, useState } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+// electron调用 NativeAPI  多进程架构
+// Inter-Process Communication，进程间通信 
+// electron 不支持import  
+// create-react-app  0配置启动 reject 
 
+
+import { ipcRenderer, remote } from 'electron'; // 去到IPC 
+const { Menu, MenuItem } = remote;
 function App() {
+  const [localCode, setLocalCode] = useState('');
+  const [controlText, setControlText] = useState('');
+
+  const login = async () => {
+    let code = await ipcRenderer.invoke('login');
+    setLocalCode(code)
+  }
+  useEffect(() => {
+    login();
+  }, []);
+  const handleContextMenu = (e) => {
+    e.preventDefault();
+    const menu = new Menu();
+    menu.append(new MenuItem({
+      label: '复制',
+      role: 'copy'
+    }))
+    menu.append(new MenuItem({
+      label: '分享到微信',
+      click: (menuItem, win, keyboardEvent) => {
+        ipcRenderer.send('share-to-wechat', '123123')
+      }
+    }))
+    menu.popup()
+  }
   return (
     <div className="App">
-      xiaop
+      {
+        controlText === '' ? <>
+          <div>你的控制码</div>
+          <span>{localCode}</span>
+        </> :
+          <div>{controlText}</div>
+      }
+        <h1 onContextMenu={(e) =>handleContextMenu(e)}>Hello World!</h1>
     </div>
   );
 }
 
+
 export default App;
+
--- electron/electron_control/main/index.js
+++ electron/electron_control/main/index.js
@@ -1,4 +1,5 @@
 let { app, BrowserWindow } = require('electron'); // node 端运行
+const handleIPC = require('./ipc'); //ipc 模块化
 let win
 app.on('ready', () => {
   win = new BrowserWindow({
@@ -8,5 +9,6 @@ app.on('ready', () => {
       nodeIntegration: true
     }
   })  // chromium 就启动了 桌面的代表就是窗口
+  handleIPC();
   win.loadURL('http://localhost:3000')
 })
