22c42de2872708bdad95f1d81e984f5e9ad17668
zby-66 <1753058819@qq.com>
2020-06-24 21:41:36 +0800

    100题

---
+++ interview/js/100题/类数组相关题目/1.js
@@ -0,0 +1,18 @@
+// var name = "1";
+
+// var obj = {
+//   name: "2",
+//   test: function () {
+//     setTimeout(function()  {
+//       var name = "3";
+//       console.log(this.name);
+//     }, 1);
+//   }
+// }
+// obj.test();
+
+console.log(typeof sayHello);
+sayHello();
+function sayHello() {
+  console.log('1')
+}
